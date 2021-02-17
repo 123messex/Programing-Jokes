3514a35f29b585035f1783a6f7270ea5d1a8b56d
Michael Essex <123messex@gmail.com>
2021-02-17 10:51:37 -0600

    Initial Commit

---
+++ Programing Jokes/Programing_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programing_JokesApp.swift
+//  Programing Jokes
+//
+//  Created by Student on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Programing_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
