c2f21dd402fb16c2e4f890590d5f9e935b54d23a
Michael Essex <123messex@gmail.com>
2021-02-25 15:48:59 -0600

    Added API query

--- Programing Jokes/ContentView.swift
+++ Programing Jokes/ContentView.swift
@@ -26,7 +26,23 @@ struct ContentView: View {
 
     }
     func getJokes() {
-        let apiKey = "?rapidapi-key= 42b30bc2c7mshd4f8918bdf9e17ep1df390jsn6fab5580756c"
+        let apiKey = "?rapidapi-key=42b30bc2c7mshd4f8918bdf9e17ep1df390jsn6fab5580756c"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        if let url = URL(string: query) {
+                 if let data = try? Data(contentsOf: url) {
+                     let json = try! JSON(data: data)
+                     if json["success"] == true {
+                        let contents = json["body"].arrayValue
+                        for item in contents {
+                                                let setup = item["setup"].stringValue
+                                                let punchline = item["punchline"].stringValue
+                                                let joke = Joke(setup: setup, punchline: punchline)
+                                                jokes.append(joke)
+                                            }
+                     }
+                 }
+             }
+
     }
 }
 
