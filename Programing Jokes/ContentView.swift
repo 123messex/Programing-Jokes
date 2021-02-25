357f55af821ffc1aa8a435bdd79e48afea33a1ec
Michael Essex <123messex@gmail.com>
2021-02-25 15:42:12 -0600

    added API Key

--- Programing Jokes/ContentView.swift
+++ Programing Jokes/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var jokes = [Joke]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(jokes) { joke in
+                NavigationLink(
+                    destination: Text(joke.punchline),
+                    label: {
+                        Text(joke.setup)
+                })
+            }
+            .navigationTitle("Programming Jokes")
+        }
+        .onAppear(perform: {
+                    getJokes()
+                })
+
+    }
+    func getJokes() {
+        let apiKey = "?rapidapi-key= 42b30bc2c7mshd4f8918bdf9e17ep1df390jsn6fab5580756c"
     }
 }
 
@@ -18,4 +34,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct Joke: Identifiable {
+    let id = UUID()
+    var setup: String
+    var punchline: String
 }
