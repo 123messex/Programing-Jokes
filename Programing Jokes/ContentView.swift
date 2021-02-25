7965d241828a265bade12cf3a6dd427d61b81f74
Michael Essex <123messex@gmail.com>
2021-02-25 15:56:20 -0600

    Finished THE THIIIIING

--- Programing Jokes/ContentView.swift
+++ Programing Jokes/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [Joke]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView{
             List(jokes) { joke in
@@ -16,33 +17,41 @@ struct ContentView: View {
                     destination: Text(joke.punchline),
                     label: {
                         Text(joke.setup)
-                })
+                    })
             }
             .navigationTitle("Programming Jokes")
         }
         .onAppear(perform: {
-                    getJokes()
-                })
-
+            getJokes()
+            
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        })
+        
     }
     func getJokes() {
         let apiKey = "?rapidapi-key=42b30bc2c7mshd4f8918bdf9e17ep1df390jsn6fab5580756c"
         let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
         if let url = URL(string: query) {
-                 if let data = try? Data(contentsOf: url) {
-                     let json = try! JSON(data: data)
-                     if json["success"] == true {
-                        let contents = json["body"].arrayValue
-                        for item in contents {
-                                                let setup = item["setup"].stringValue
-                                                let punchline = item["punchline"].stringValue
-                                                let joke = Joke(setup: setup, punchline: punchline)
-                                                jokes.append(joke)
-                                            }
-                     }
-                 }
-             }
-
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["setup"].stringValue
+                        let punchline = item["punchline"].stringValue
+                        let joke = Joke(setup: setup, punchline: punchline)
+                        jokes.append(joke)
+                    }
+                    return
+                }
+            }
+        }
+        showingAlert = true
+        
     }
 }
 
